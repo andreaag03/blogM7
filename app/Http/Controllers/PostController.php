08a99bde511e0c044b6815b95defcff033979e8e
Albert Cervelló <albertcervellogarci@gmail.com>
2022-05-06 18:02:41 +0200

    Changed tabs

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::paginate();
+
         return view('post.index', compact('posts'));
     }
 
@@ -48,6 +49,7 @@ class PostController extends Controller
     public function show(int $id)
     {
         $post = Post::find($id);
+
         return view('post.show', compact('post'));
     }
 
